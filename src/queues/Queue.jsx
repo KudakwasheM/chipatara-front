09e02032cb4743d217e261693bf3e97a8bf0718d
Kudakwashe Masaya <masayakudakwashe@gmail.com>
2024-05-15 21:45:40 +0200

    Update Queue.jsx

--- src/queues/Queue.jsx
+++ src/queues/Queue.jsx
@@ -30,6 +30,30 @@ const Queue = () => {
     }
   };
 
+  const attend = async (index) => {
+    try {
+      setLoading(true);
+
+      const body = {
+        position: index,
+        attended: true,
+      };
+      const res = axiosClient.put("/queues/attend", body);
+      toast.success("Patient attended");
+      getQueue();
+    } catch (err) {
+      if (err.response.status === 400) {
+        toast.warn(err.response.data.error);
+      } else if (err.code === "ERR_BAD_RESPONSE") {
+        toast.error("Internal Server Error");
+      } else {
+        toast.error("An error occured");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const remove = async (patient_id) => {
     try {
       const pId = {
@@ -37,7 +61,6 @@ const Queue = () => {
       };
 
       const res = await axiosClient.put("/queues/remove", pId);
-      console.log(res);
       toast.success("Patient removed");
       getQueue();
     } catch (err) {
@@ -48,6 +71,8 @@ const Queue = () => {
       } else {
         toast.error("An error occured");
       }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -74,9 +99,9 @@ const Queue = () => {
                 <div className="row">
                   {patients.length > 0 ? (
                     <>
-                      {patients.map((patient) => {
+                      {patients.map((patient, index) => {
                         return (
-                          <div className="col-sm-3 col-12">
+                          <div className="col-sm-3 col-12" key={index}>
                             <div className="card shadow mb-4">
                               <div className="card-header">
                                 <h5 className="card-title">
@@ -88,21 +113,29 @@ const Queue = () => {
                                   <h4>{patient.name}</h4>
 
                                   <div className="row">
-                                    {userInfo.role === "super" ? (
-                                      <div className="d-flex gap-1">
+                                    {userInfo.role === "super" || "doctor" ? (
+                                      <div className="d-flex justify-content-center gap-1">
                                         <Link
-                                          className="btn btn-success col-6"
+                                          className="btn btn-primary col-3"
                                           to={`/${userInfo.role}/visits/create/${patient.patient_id}`}
                                         >
-                                          Attend
+                                          <i className="bi bi-eye m-0"></i>
                                         </Link>
                                         <button
-                                          className="btn btn-danger col-6"
+                                          className="btn btn-success col-3"
+                                          onClick={() =>
+                                            attend(patient.position)
+                                          }
+                                        >
+                                          <i className="bi bi-hand-thumbs-up"></i>
+                                        </button>
+                                        <button
+                                          className="btn btn-danger col-3"
                                           onClick={() =>
                                             remove(patient.patient_id)
                                           }
                                         >
-                                          Remove
+                                          <i className="bi bi-x"></i>
                                         </button>
                                       </div>
                                     ) : (
