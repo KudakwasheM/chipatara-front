975d6180e83e74df5c12e916373dc6bf31f063ac
Kudakwashe Masaya <masayakudakwashe@gmail.com>
2024-06-18 09:12:33 +0200

    Update Visit.jsx

--- src/visits/Visit.jsx
+++ src/visits/Visit.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import axiosClient from "../utils/axiosClient";
 import CustomLoader from "../components/CustomLoader";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Visit = () => {
   const { id } = useParams();
   const [visit, setVisit] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.auth);
 
   const getVisit = async () => {
     try {
@@ -33,6 +38,7 @@ const Visit = () => {
   if (visit === null) {
     return <CustomLoader />;
   }
+
   return (
     <div className="app-body">
       <div className="row">
@@ -44,9 +50,87 @@ const Visit = () => {
             {loading ? (
               <CustomLoader />
             ) : (
-              <div className="card-body">
-                <div className="row"></div>
-              </div>
+              <>
+                <div className="card-body">
+                  <div className="row">
+                    <h4>{visit.patient.name}</h4>
+                  </div>
+                  <hr />
+                  <div className="row mb-4">
+                    <div className="col col-sm12 col-6">
+                      <label htmlFor="">
+                        <h4>Symptoms:</h4>
+                      </label>
+                      <textarea
+                        className="form-control"
+                        value={visit.symptoms}
+                        rows={3}
+                      ></textarea>
+                    </div>
+                    <div className="col col-sm12 col-6 ml-2">
+                      <label htmlFor="">
+                        <h4>Diagnosis:</h4>
+                      </label>
+                      <textarea
+                        className="form-control"
+                        value={visit.diagnosis}
+                        rows={3}
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col col-sm12 col-6 ml-2">
+                      <label htmlFor="">
+                        <h4>Treatment:</h4>
+                      </label>
+                      <textarea
+                        className="form-control"
+                        value={visit.treatment}
+                        rows={3}
+                      ></textarea>
+                    </div>
+                  </div>
+                  <hr />
+                  {visit.prescription === null ? (
+                    <div className="row">
+                      <label htmlFor="">
+                        <h4>Prescription</h4>
+                      </label>
+                      <p>No prescription</p>
+                    </div>
+                  ) : (
+                    <div className="row">
+                      <label htmlFor="">
+                        <h4>Prescription</h4>
+                      </label>
+                      <Link>
+                        Show prescription{" "}
+                        {visit.prescription.prescription_number}
+                      </Link>
+                    </div>
+                  )}
+                </div>
+                <div className="card-footer">
+                  <div className="d-flex gap-2 justify-content-end">
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => navigate(-1)}
+                    >
+                      Back
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={() =>
+                        navigate(`/${userInfo.role}/visits/edit/${visit._id}`)
+                      }
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              </>
             )}
           </div>
         </div>
